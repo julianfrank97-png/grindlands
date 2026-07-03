c966ace721c12021f34c667a1e4f8e2d1d277c8e
julianfrank97-png <julianfrank97@gmail.com>
2026-07-04 00:45:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "grindlands-v2";
+const CACHE = "grindlands-v4";
 const ASSETS = ["./", "./index.html", "./bundle.js", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
